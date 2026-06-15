d2051ed4560a225e3fda52b096ba3e2032e6d410
Gaurav Bhattacharjee <gauravbhattacharjee54@gmail.com>
2026-06-16 00:10:49 +0530

    Update auth.py

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,1 +1,12 @@
+from pydantic import BaseModel
 
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+
+class RegisterRequest(BaseModel):
+    username: str
+    email: str
+    password: str
